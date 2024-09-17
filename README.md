e04797760381324b7421488bb9c61247bd41524d
Jordan <jordan.feng@hotmail.com>
2024-09-17 18:43:26 -0400

    Add instructions and small fix

---
+++ README.md
@@ -0,0 +1,8 @@
+# Slots Game
+Welcome! This is a game I thought up from a spur of the moment. Here's how you can play it!
+
+### Instructions
+1. Clone the repo onto your local machine
+2. From the Terminal, go to the directory of the game files
+3. Run `python game.py`
+4. Enjoy! Note you can also adjust the game parameters in `game.py` before starting the game.
--- game.py
+++ game.py
@@ -1,13 +1,23 @@
 from slots import *
 
+# GAME PARAMS: Change to your preference
+initialStartingBalance = 1000
+slotOptions = [5, 6]
+
 # Create a new slot machine before starting the game
-slotMachine = SlotMachine()
+slotMachine = SlotMachine(balance=initialStartingBalance, slotOptions=slotOptions)
 
 while True:
     if (slotMachine.balanceIsZero() == False):
         if (slotMachine.getGambleAmount() == True):
             slotMachine.animateSlotRoll()
+
             [slotOne, slotTwo, slotThree] = slotMachine.rollSlots()
+            slotMachine.clearScreen()
+            print('[' + str(slotOne) + '] [' + 
+                  str(slotTwo) + '] [' + 
+                  str(slotThree) +']')
+            
             print(slotMachine.calculateWinLoss([slotOne, slotTwo, slotThree]))
         else: 
             slotMachine.clearScreen()
