c4d017e79b626293baa9eaac8f7b60b706163316
Jordan <jordan.feng@hotmail.com>
2024-09-17 17:31:28 -0400

    Add comments and clear screen function

--- slots.py
+++ slots.py
@@ -2,20 +2,25 @@ import random
 import os
 import time
 
-slotOptions = [1, 2, 3, 4, 5, 6]
+slotOptions = [1, 2, 3]
 balance = 1000
 
+# Clear the command prompt screen based on OS
+def clearScreen():
+    if (os.name == 'nt'):
+        os.system('cls')
+    elif (os.name == 'posix'):
+        os.system('clear')
+
 while True:
+    # Quit game loop once user has no money left
     if balance == 0:
         print('You have no money left, Game Over')
         break
 
     print('How much do you want to gamble? Your balance is ' + str(balance))
     gamble = input()
-    if (os.name == 'nt'):
-        os.system('cls')
-    elif (os.name == 'posix'):
-        os.system('clear')
+    clearScreen()
 
     if (gamble.isdigit() == True):
         if (int(gamble) > balance):
@@ -25,20 +30,18 @@ while True:
             slotTwo = random.choice(slotOptions)
             slotThree = random.choice(slotOptions)
 
+            # 'Animate' slots
             for i in range(0, 5):
-                if (os.name == 'nt'):
-                    os.system('cls')
-                elif (os.name == 'posix'):
-                    os.system('clear')
+                clearScreen()
                 print('[' + str(random.choice(slotOptions)) + '] [' + str(random.choice(slotOptions)) + '] [' + str(random.choice(slotOptions)) +']')
-                time.sleep(0.3)
-            if (os.name == 'nt'):
-                os.system('cls')
-            elif (os.name == 'posix'):
-                os.system('clear')
+                time.sleep(0.2)
+
+            clearScreen()
             print('[' + str(slotOne) + '] [' + str(slotTwo) + '] [' + str(slotThree) +']')
 
+            # Recalculate balance based on win/loss
             if (slotOne == slotTwo and slotTwo == slotThree):
+                # Prize calculated by slot number * gamble value
                 wins = int(slotOne) * int(gamble)
                 balance += int(slotOne) * int(gamble)
                 print('Congratulations you have won ' + str(wins) + ' credits!')
